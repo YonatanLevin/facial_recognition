c3ee4dbe8c8c11dc4fbbd4776919201e4a809e66
YonatanLevin <yoni1.levin@gmail.com>
2025-12-11 15:44:16 +0200

    setup venv

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 def main():
     print('starting pipeline')
+    print('pipeline ended')
 
 if __name__ == '__main__':
     main()
--- siamese_encoders/encoder.py
+++ siamese_encoders/encoder.py
@@ -1,4 +1,14 @@
 import ABS
+from typing import Any
+
 
 class Encoder(ABS):
-    pass
+    
+    def __init__(self, hyper_parameters: dict[str, Any]):
+        self.hyper_parameters = hyper_parameters
+
+    def forwads():
+        pass
+
+    def backwards():
+        pass
