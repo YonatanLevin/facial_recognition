23a0da5832fc2661de019f9f674f1c4df0b88f64
YonatanLevin <yoni1.levin@gmail.com>
2025-12-28 09:30:09 +0200

    Fixed kaggle data import

--- src/my_package/constants.py
+++ src/my_package/constants.py
@@ -1,3 +1,4 @@
+from os.path import isdir
 from pathlib import Path
 
 # Points to the project root (where history.csv lives)
@@ -6,4 +7,7 @@ BASE_DIR = Path(__file__).resolve().parents[2]
 # Specific file paths
 HISTORY_PATH = BASE_DIR / "history.csv"
 HISTORY_PLOTS_DIR = BASE_DIR / "history_plots"
-DATA_DIR = BASE_DIR / "data"
+kaggle_data_dir = '/kaggle/input/face-recognition-data/data'
+local_data_dir = BASE_DIR / "data"
+
+DATA_DIR = kaggle_data_dir if isdir(kaggle_data_dir) else local_data_dir
